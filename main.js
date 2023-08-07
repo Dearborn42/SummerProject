cb8110d190521b58ece5ecf115ec69bf6e6f0851
Dearborn42 <amurf26@outlook.com>
2023-08-07 13:53:50 -0700

    finished dark mode and added pictures

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let i=0;
-let events = ["pic1", "pic2", "pic3"];
+let events = ["I hung out with my girlfriend throughout the summer. We went out to dinner, the gym, to each other's house, on car rides, and numerous other activities.", "Over the summer I got a flat tire and had to fix it. During the process my wrench broke and I had to wait for my dad to get me a new one. The jack that sat out in the sun started to melt the asphalt of the road from the Arizona heat.", "Over the summer I started to go to a different gym with my friends. We spent have been going every day."];
 
 $(".carousel-control-next").on("click", ()=>{
     if(i<2) i++;
